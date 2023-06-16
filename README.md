3e83396079d60b75a257ae407c62b76b6b9edd69
ASvich <ASvich@qulix.com>
2023-06-16 15:31:20 +0300

    change documentation

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# code-otp
+# Ввода кода из СМС в браузерах с поддержкой WebOTP Api
 
-Проверка работы компонента CodeInput из библиотеки **core-components** (https://core-ds.github.io/core-components/master/?path=/docs/components-codeinput--docs).
+### Проверка работы компонента CodeInput из библиотеки **core-components** (https://core-ds.github.io/core-components/master/?path=/docs/components-codeinput--docs).
+###Подробнее о WebOTP Api: https://developer.chrome.com/articles/web-otp
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import {CodeInput} from "@alfalab/core-components/code-input";
+import {Link} from "@alfalab/core-components/link";
 import './App.css';
 
 function App() {
     return (
         <div className="app">
-            <div>Ввод кода otp</div>
+            <div>Ввод кода из СМС в браузерах с поддержкой WebOTP Api.</div>
+            <div>Для автоматической подстановки кода необходимо отправить смс на свой телефон формата:</div>
+            
+            <hr/>
+            <div style={{display: 'inline-flex', flexDirection: 'column', textAlign: 'left'}}>
+                <p>Your OTP is: 1234.</p>
+                <p>@main--code-otp.netlify.app #1234</p>
+            </div>
+            <hr/>
             
             <CodeInput className={'alfaCodeInput'}/>
+            
+            <div style={{marginTop: '50px'}}>
+                <Link href={'https://developer.chrome.com/articles/web-otp'} target={"_blank"} view={'secondary'}>
+                    Подробнее о WebOTP Api
+                </Link>
+            </div>
         </div>
     );
 }
