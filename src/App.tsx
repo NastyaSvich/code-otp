5ea2fe126f5eaf28c59a3f8d54703d7cf75cfe61
ASvich <ASvich@qulix.com>
2023-06-16 16:15:46 +0300

    change page view

--- src/App.tsx
+++ src/App.tsx
@@ -11,20 +11,20 @@ function App() {
                 (номера отправителя не должно быть в контактах) формата:
             </div>
             
-            <hr/>
-            <div style={{display: 'inline-flex', flexDirection: 'column', textAlign: 'left'}}>
-                <p>Your OTP is: 1234.</p>
-                <p>@code-otp.netlify.app #1234</p>
-            </div>
-            <hr/>
+            <pre className={'codeBlock'}>
+                <code className="language-text">
+                    Your OTP is: 1234.
+                    <br/>
+                    <br/>
+                    @main--code-otp.netlify.app #1234
+                </code>
+            </pre>
             
             <CodeInput className={'alfaCodeInput'}/>
             
-            <div style={{marginTop: '50px'}}>
-                <Link href={'https://developer.chrome.com/articles/web-otp'} target={"_blank"} view={'secondary'}>
-                    Подробнее о WebOTP Api
-                </Link>
-            </div>
+            <Link className={'alfaLink'} href={'https://developer.chrome.com/articles/web-otp'} target={"_blank"} view={'secondary'}>
+                Подробнее о WebOTP Api
+            </Link>
         </div>
     );
 }
